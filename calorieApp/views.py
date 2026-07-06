1069e74e46d0b579a18ac975a783c7349bd7cf41
amor <amorsaralynn@gmail.com>
2026-07-06 07:47:31 +0300

    create a view for displaying foods and total calories

--- calorieApp/views.py
+++ calorieApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from .models import *
 
 # Create your views here.
+def home(request):
+    foods = Food.objects.all()
+    total_calories = sum(food.calories for food in foods)
+
+    context = {
+        "foods": foods,
+        "total_calories": total_calories,
+    }
+
+    return render(request, "home.html", context)
